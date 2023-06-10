391aa5d98ad3d19d7b5196a3790712fcfacaabc3
Olga Polishchuk <olgapolichuk@ukr.net>
2023-06-10 17:05:34 +0300

    add serch

--- controllers/recipes/getAll.js
+++ controllers/recipes/getAll.js
@@ -1,12 +1,42 @@
 const { Recipe } = require("../../models/recipe");
+const { Ingredient } = require("../../models/ingredients");
+const { RequestError } = require("../../helpers");
 
 const getAll = async (req, res) => {
-console.log("+");
-  const conditions = {};
-//   const { page = 1, limit = 10 } = req.query;
-//   const skip = (page - 1) * limit;
-  const result = await Recipe.find({});
-  res.json(result);
-};
+  const { path } = req.route;
+  const { filter } = req.query;
+
+  // const { page, skip, limit } = req.paginatedResponse;
+
+  const searchParams = {};
+
+  if (filter?.title) searchParams.title = filter.title;
+  
+  if (filter?.ingredients) {
+    const ingTitle = filter?.ingredients
+    
+    const ingredients = await Ingredient.find(
+      { name: { $regex: ingTitle, $options: "i" } },
+      { _id: 1 }
+      );
+      
+      searchParams.ingredients = ingredients;
+    }
+    
+    // console.log(searchParams);
+    
+    const response = await Recipe.find(searchParams, '-createdAt -updatedAt', 
+    // {skip, limit}
+    ); 
+
+    // console.log(response);
+    res.status(200).json(response );
+    
+    if (!response) {
+      throw RequestError(404, 'Not found')
+    }
+  }
 
 module.exports = getAll;
+
+
