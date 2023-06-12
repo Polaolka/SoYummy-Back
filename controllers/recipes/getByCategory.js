fd62d5bfec4b4a49eaacbc4c2081788add186bbd
Olga Polishchuk <olgapolichuk@ukr.net>
2023-06-12 23:12:30 +0300

    fix response

--- controllers/recipes/getByCategory.js
+++ controllers/recipes/getByCategory.js
@@ -8,16 +8,16 @@ const getByCategory = async (req, res) => {
   const categoryRegex = new RegExp(category, "i");
 
   const count = await Recipe.countDocuments({ category: categoryRegex });
-  const respons = await Recipe.find({ category: categoryRegex }, null, {
+  const response = await Recipe.find({ category: categoryRegex }, null, {
     skip,
     limit,
   }).populate("ingredients.id");
 
-  if (!respons) {
+  if (!response) {
     res.status(404).json({ message: "Not found" });
   }
 
-  res.status(200).json({respons, total: count });
+  res.status(200).json({response, total: count });
 };
 
 module.exports = getByCategory;
