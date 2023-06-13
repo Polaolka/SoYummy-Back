1df871f19c6a833e49a02438e9bb97debfc2e1ae
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-06-13 19:14:24 +0300

    вв

--- models/recipe.js
+++ models/recipe.js
@@ -39,7 +39,7 @@ recipeSchema.set("versionKey", false);
 const addNewSchema = Joi.object({
   title: Joi.string().required().min(2),
   category: Joi.string().required().min(2),
-  instructions: Joi.array().required().min(2),
+  instructions: Joi.string().required().min(2),
   description: Joi.string().required().min(2),
   thumb: Joi.string(),
   preview: Joi.string(),
